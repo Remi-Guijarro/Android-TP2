34a8aaeaebc1f9a18fbb31c3a8bf198787a4b8c7
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-11-23 15:20:35 +0100

    Added fragment task dedicated layout

--- app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
@@ -1,7 +1,17 @@
 package com.example.todogeoffreyremi.tasklist
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class TaskListFragment : Fragment() {
-
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
 }
