4265b42e160c2aacb1d4a476520f5eda04190d2d
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-11-23 17:58:21 +0100

    Fixed layout issues and end of TP2

--- app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
@@ -8,9 +8,11 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.todogeoffreyremi.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.util.*
 
 class TaskListFragment : Fragment() {
-    private val taskList = listOf(  Task(id = "id_1", title = "Task 1", description = "description 1"),
+    private val taskList = mutableListOf(Task(id = "id_1", title = "Task 1", description = "description 1"),
         Task(id = "id_2", title = "Task 2"),
         Task(id = "id_3", title = "Task 3"))
 
@@ -19,7 +21,8 @@ class TaskListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_task_list, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_task_list, container, false)
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,5 +30,11 @@ class TaskListFragment : Fragment() {
         var recyclerView = view.findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.layoutManager = LinearLayoutManager(activity)
         recyclerView.adapter = TaskListAdapter(taskList)
+        val addButton = view.findViewById<FloatingActionButton>(R.id.addFAB)
+        addButton.setOnClickListener {
+            val task =Task(id = UUID.randomUUID().toString(), title = "Task ${taskList.size + 1}")
+            taskList.add(task)
+            (recyclerView.adapter as TaskListAdapter).notifyItemInserted(taskList.lastIndex)
+        }
     }
 }
