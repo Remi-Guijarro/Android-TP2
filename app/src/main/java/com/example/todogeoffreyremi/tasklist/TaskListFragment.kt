78a238a2aa09f90445595e60108478ce25ac3f1b
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-11-23 15:36:22 +0100

    Inflated fragment task list layout

--- app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
+++ app/src/main/java/com/example/todogeoffreyremi/tasklist/TaskListFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.todogeoffreyremi.R
 
 class TaskListFragment : Fragment() {
     override fun onCreateView(
@@ -12,6 +13,6 @@ class TaskListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return inflater.inflate(R.layout.fragment_task_list, container, false)
     }
 }
