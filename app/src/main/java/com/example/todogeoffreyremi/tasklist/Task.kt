94ff73a778e43a46787606cdd32084423b01b403
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-11-23 17:03:43 +0100

    Created task data class

--- app/src/main/java/com/example/todogeoffreyremi/tasklist/Task.kt
+++ app/src/main/java/com/example/todogeoffreyremi/tasklist/Task.kt
@@ -1,2 +1,7 @@
 package com.example.todogeoffreyremi.tasklist
 
+data class Task (
+    private val id : String? = null,
+    private var title : String? = "",
+    private var description : String? = "",
+)
