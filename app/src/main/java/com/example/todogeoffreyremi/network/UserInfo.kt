4ad35dac9aa68f2b8a9ed6327b85a29bf3566e40
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-12-07 16:36:21 +0100

    Added service and data class for UserInfo endpoint

---
+++ app/src/main/java/com/example/todogeoffreyremi/network/UserInfo.kt
@@ -0,0 +1,15 @@
+package com.example.todogeoffreyremi.network
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserInfo(
+    @SerialName("email")
+    val email: String,
+
+    @SerialName("firstname")
+    val firstName: String,
+
+    @SerialName("lastname")
+    val lastName: String
+)
