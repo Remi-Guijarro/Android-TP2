cf6f675d8e8c02f4acd7643a7b1e029f6e930d1b
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-12-07 17:18:14 +0100

    Set task as Serializable from kotlinX perspective

--- app/src/main/java/com/example/todogeoffreyremi/network/TaskWebService.kt
+++ app/src/main/java/com/example/todogeoffreyremi/network/TaskWebService.kt
@@ -1,4 +1,10 @@
 package com.example.todogeoffreyremi.network
 
+import com.example.todogeoffreyremi.tasklist.Task
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface TaskWebService {
+    @GET("tasks")
+    suspend fun getTasks(): Response<List<Task>>
 }
--- app/src/main/java/com/example/todogeoffreyremi/tasklist/Task.kt
+++ app/src/main/java/com/example/todogeoffreyremi/tasklist/Task.kt
@@ -1,9 +1,15 @@
 package com.example.todogeoffreyremi.tasklist
 
-import java.io.Serializable
+import kotlinx.serialization.SerialName
 
+@kotlinx.serialization.Serializable
 data class Task (
+    @SerialName("id")
     val id : String? = null,
+
+    @SerialName("title")
     var title : String? = "",
+
+    @SerialName("description")
     var description : String? = "Task description",
-) : Serializable
+) : java.io.Serializable
