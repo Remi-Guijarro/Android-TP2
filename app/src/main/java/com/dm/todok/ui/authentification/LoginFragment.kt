dd19382ef800022cfbf03fc0cdc89e3708fa4dd2
Geoffrey Delval <geoffrey.dlvl@gmail.com>
2020-12-29 18:53:40 +0100

    Navigate between Login and Signup

--- app/src/main/java/com/dm/todok/ui/authentification/LoginFragment.kt
+++ app/src/main/java/com/dm/todok/ui/authentification/LoginFragment.kt
@@ -1,6 +1,23 @@
 package com.dm.todok.ui.authentification
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.dm.todok.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
+    private var _binding: FragmentLoginBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentLoginBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
--- app/src/main/java/com/dm/todok/ui/authentification/SignupFragment.kt
+++ app/src/main/java/com/dm/todok/ui/authentification/SignupFragment.kt
@@ -1,6 +1,23 @@
 package com.dm.todok.ui.authentification
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.dm.todok.databinding.FragmentSignupBinding
 
 class SignupFragment : Fragment() {
+    private var _binding: FragmentSignupBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = FragmentSignupBinding.inflate(inflater, container, false)
+        val rootView = binding.root
+        return rootView
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
