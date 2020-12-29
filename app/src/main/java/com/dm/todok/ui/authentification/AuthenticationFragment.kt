df815a7040a60d7e2c408a5287964a2e2e8eccab
Remi-Guijarro <remi.guijarro@outlook.fr>
2020-12-29 17:24:50 +0100

    Added navigation graph

--- app/src/main/java/com/dm/todok/ui/authentification/AuthenticationFragment.kt
+++ app/src/main/java/com/dm/todok/ui/authentification/AuthenticationFragment.kt
@@ -1,6 +1,13 @@
 package com.dm.todok.ui.authentification
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import com.dm.todok.R
 
 class AuthenticationFragment : Fragment() {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
 }
